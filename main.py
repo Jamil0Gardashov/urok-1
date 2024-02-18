4a32d38546c602483c1a6ad2718e33bd7dc325e4
Jamil Gardashov <157944894+Jamil0Gardashov@users.noreply.github.com>
2024-02-18 11:57:28 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-class Student:
-    def __init__(self, name, age, grade):
-        self.name = name
-        self.age = age
-        self.grade = grade
 
-    def display_info(self):
-        print(f"Имя: {self.name}, Возраст: {self.age}, Класс: {self.grade}")
-
-student1 = Student("Иван", 15, 9)
-student2 = Student("Мария", 16, 10)
-
-student1.display_info()
-student2.display_info()
